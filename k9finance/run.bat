d2fdebd4340e24a0042ab6071cd8b21f9f740da7
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-05-15 19:08:03 +0700

    Fix .env error

--- k9finance/run.bat
+++ k9finance/run.bat
@@ -8,14 +8,12 @@ cd /d "%~dp0"
 REM Ensure necessary files exist or create them
 if not exist proxies.txt echo proxies.txt created & type nul > proxies.txt
 if not exist newPrivateKeys.txt echo newPrivateKeys.txt created & type nul > newPrivateKeys.txt
-if not exist .env echo .env created & (
-    echo MAX_THREADS=10
-    echo SLEEP_TIME=2
-    echo DEFAULT_WALLETS=100
-    echo TIMEOUT=10
-    echo REFERRAL_CODES=ABC123,DEF456
-    echo SKIP_ERROR_PROXIES=False
-) > .env
+if not exist .env (
+	echo Copying configuration file
+	copy .env-example .env
+) else (
+	echo Skipping .env copying
+)
 
 pip install -r requirements.txt
 python meomundep.py
